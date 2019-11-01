ce210894779ec9fe5ce653c36400aae903326fd0
howard0303 <howard010203@gmail.com>
2019-11-01 15:57:56 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,31 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("輸入性別ex: man/woman");
+        String ss = sc.next();
+        System.out.println("輸入年齡:");
+        int age = sc.nextInt();
+        if(ss.equalsIgnoreCase("man")){
+            if(age>=18){
+                System.out.println("能結婚");
+            }else{
+                System.out.println("不能結婚");
+            }
+
+        }else if(ss.equalsIgnoreCase("woman")){
+            if(age>=16){
+                System.out.println("能結婚");
+            }else{
+                System.out.println("不能結婚");
+            }
+
+        }else{
+            System.out.println("性別輸入錯誤");
+        }
+
+    }
+}
